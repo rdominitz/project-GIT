c9972503266a7eb3a580533380b9c3c77b754f17
galmor1007 <galmor1007@gmail.com>
2017-04-03 16:32:03 +0300

    Add files via upload

---
+++ ServerImpl/communication/Controllers/ManageGroupController.cs
@@ -0,0 +1,60 @@
+﻿using communication.Core;
+using communication.Models.Group;
+using Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace communication.Controllers
+{
+    public class ManageGroupController : Controller
+    {
+        // GET: manageGroup
+        [HttpGet]
+        public ActionResult Index(string message)
+        {
+            HttpCookie cookie = Request.Cookies["userId"];
+            if (cookie == null)
+            {
+                return RedirectToAction("Index", "Login", new { message = "you were not logged in. please log in and then try again" });
+            }
+            ViewBag.message = message;
+
+            return View(getData(Convert.ToInt32(cookie.Value)));
+        }
+
+        [HttpPost]
+        public ActionResult Submit(string groupName)
+        {
+            HttpCookie cookie = Request.Cookies["userId"];
+            if (cookie == null)
+            {
+                return RedirectToAction("Index", "Login", new { message = "you were not logged in. please log in and then try again" });
+            }
+
+            ViewBag.groupName = groupName;
+
+            string ans = ServerWiring.getInstance().removeGroup(Convert.ToInt32(cookie.Value), groupName);
+            if (ans.Equals(Replies.SUCCESS))
+            {
+                return RedirectToAction("Index", "Main");
+            }
+            return RedirectToAction("Index", "ManageGroup", new { message = ans });
+        }
+
+        List<GroupData> getData(int adminId)
+        {
+            List<GroupData> data = new List<GroupData>();
+            Tuple<string, List<string>> groups = ServerWiring.getInstance().getAllAdminsGroups(adminId);
+            // verify success
+            foreach (string group in groups.Item2)
+            {
+                data.Add(new GroupData(group));
+            }
+            return data;
+        }
+
+    }
+}
