9d1fb3d9147f4a20942ab77a52b5e60ab906cf05
avivasido <aviv22a@gmail.com>
2017-04-27 12:57:03 +0300

    administration

---
+++ ServerImpl/communication/Controllers/administrationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace communication.Controllers
+{
+    public class administrationController : Controller
+    {
+        //
+        // GET: /administration/
+        [HttpGet]
+        public ActionResult Index(string message)
+        {
+            if (message != null)
+            {
+                ViewBag.message = message;
+            }
+
+            HttpCookie cookie = Request.Cookies["userId"];
+            if (cookie == null)
+            {
+                return RedirectToAction("Index", "Login", new { message = "you were not logged in. please log in and then try again" });
+            }
+
+            return View();
+        }
+	}
+}
